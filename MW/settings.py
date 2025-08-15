0a4d1036ab77eefc10cc8319a092d2cceef5cfb2
jackbland3k <jbb43@njit.edu>
2025-08-15 14:04:50 -0400

    changed debug from false to os.environ.get... to check why server 500 is still occuring

--- MW/settings.py
+++ MW/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get(
     )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DJANGO_DEBUG", "0") == "1"
 
 ALLOWED_HOSTS = [   
     "michaels-website-1.onrender.com",]
