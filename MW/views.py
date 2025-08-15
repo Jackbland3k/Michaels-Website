0d19a65cd6adccc1cef7ebc0dc61ccc0314c3b78
jackbland3k <jbb43@njit.edu>
2025-08-15 13:58:16 -0400

    Fixed case sens issues again and made a more abs path for static dir

--- MW/views.py
+++ MW/views.py
@@ -9,16 +9,16 @@ def home_view(request):
 
 # 2. CV View
 def CV_view(request):
-    return render(request, 'CV.html')
+    return render(request, 'cv.html')
 
 # 3. Research View
 def Research_view(request):
-    return render(request, 'Research.html')
+    return render(request, 'research.html')
 
 # 4. Teaching View
 def Teaching_view(request):
-    return render(request, 'Teaching.html')
+    return render(request, 'teaching.html')
 
 # 5. Home View
 def Contact_view(request):
-    return render(request, 'Contact.html')
+    return render(request, 'contact.html')
